642d8fff4aa0611a3559d06f3d445e706fe6ebbe
Scott Watermasysk <scottwater@gmail.com>
2026-01-25 21:22:46 -0500

    Harden update generator

--- lib/local_ci_plus/generators/update_generator.rb
+++ lib/local_ci_plus/generators/update_generator.rb
@@ -6,19 +6,35 @@ module LocalCiPlus
   module Generators
     class UpdateGenerator < Rails::Generators::Base
       def update_ci_runner
-        if File.exist?(File.join(destination_root, "bin/ci"))
-          unless ci_requires_local_ci_plus?
-            inject_into_file "bin/ci", "require \"local_ci_plus\"\n", after: "require_relative \"../config/boot\"\n"
-          end
-        else
+        ci_path = File.join(destination_root, "bin/ci")
+
+        unless File.exist?(ci_path)
           say_status :skipped, "bin/ci does not exist", :yellow
+          return
         end
+
+        return if ci_requires_local_ci_plus?
+
+        lines = File.read(ci_path).lines
+        boot_index = lines.find_index { |line| line.match?(boot_require_regex) }
+
+        unless boot_index
+          say_status :skipped, "bin/ci does not require config/boot", :yellow
+          return
+        end
+
+        lines.insert(boot_index + 1, "require \"local_ci_plus\"\n")
+        File.write(ci_path, lines.join)
       end
 
       private
 
       def ci_requires_local_ci_plus?
-        File.read(File.join(destination_root, "bin/ci")).include?("require \"local_ci_plus\"")
+        File.read(File.join(destination_root, "bin/ci")).match?(/require\s+["']local_ci_plus["']/)
+      end
+
+      def boot_require_regex
+        /^\s*require_relative\s+\(?\s*["']\.\.\/config\/boot["']/
       end
     end
   end
--- test/generators/update_generator_test.rb
+++ test/generators/update_generator_test.rb
@@ -29,6 +29,36 @@ class UpdateGeneratorTest < Rails::Generators::TestCase
     assert_equal 1, File.read(destination_root + "/bin/ci").scan('require "local_ci_plus"').size
   end
 
+  def test_update_handles_single_quoted_boot_require
+    create_file "bin/ci", <<~RUBY
+      #!/usr/bin/env ruby
+      # frozen_string_literal: true
+
+      require_relative '../config/boot'
+      require "active_support/continuous_integration"
+    RUBY
+
+    run_generator
+
+    contents = File.read(destination_root + "/bin/ci")
+    assert_match(/require_relative '..\/config\/boot'\nrequire "local_ci_plus"/, contents)
+  end
+
+  def test_update_handles_boot_require_with_whitespace_and_comments
+    create_file "bin/ci", <<~RUBY
+      #!/usr/bin/env ruby
+      # frozen_string_literal: true
+
+      require_relative   "../config/boot"   # boot the app
+      require "active_support/continuous_integration"
+    RUBY
+
+    run_generator
+
+    contents = File.read(destination_root + "/bin/ci")
+    assert_match(/require_relative\s+\"..\/config\/boot\".*\nrequire "local_ci_plus"/, contents)
+  end
+
   def test_update_is_idempotent
     create_file "bin/ci", <<~RUBY
       #!/usr/bin/env ruby
@@ -42,6 +72,21 @@ class UpdateGeneratorTest < Rails::Generators::TestCase
     run_generator
 
     assert_equal 1, File.read(destination_root + "/bin/ci").scan('require "local_ci_plus"').size
+  end
+
+  def test_update_skips_when_no_boot_require
+    create_file "bin/ci", <<~RUBY
+      #!/usr/bin/env ruby
+      # frozen_string_literal: true
+
+      require "active_support/continuous_integration"
+    RUBY
+
+    output = run_generator
+
+    refute_match(/require "local_ci_plus"/, File.read(destination_root + "/bin/ci"))
+    assert_match(/skipped/i, output)
+    assert_match(/config\/boot/, output)
   end
 
   private
